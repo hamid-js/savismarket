0bd062917b93e774734862f4b74733cc24107b22
Hamidreza Mohamadi <hamid.reactjs@gmail.com>
2024-01-24 14:45:18 +0330

    create pricing table  and pricingbox  section

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,8 @@ function App() {
         <Silder />
         <MobileMenu />
         <ProductsSection />
+        <ProductCarousel />
+        <PricingBox />
       </main>
     </div>
   );
@@ -114,12 +116,12 @@ function MobileMenu() {
 
 function ProductCard() {
   return (
-    <div className="w-[47%] text-white m-1 ">
+    <div className="m-1 w-[47%] text-white ">
       <div className="flex  h-40 w-full  flex-col justify-between bg-[url(/images/digital/Image_48.jpg)] bg-cover bg-center  ">
         <div className="mr-2 mt-1 h-8 w-8 self-end  rounded-full bg-black p-2">
           <RiHeartLine className=" " />
         </div>
-        <div className="text- h-7 w-7 bg-blue-500 rounded-sm">
+        <div className="text- h-7 w-7 rounded-sm bg-blue-500">
           <RiShoppingCartLine className="-scale-x-100  pr-1  pt-2 text-2xl  " />
         </div>
       </div>
@@ -134,7 +136,7 @@ function ProductCard() {
 
 function ProductsSection() {
   return (
-    <section  className='flex w-full justify-around flex-wrap '>
+    <section className="flex w-full flex-wrap justify-around ">
       <ProductCard />
       <ProductCard />
       <ProductCard />
@@ -145,5 +147,70 @@ function ProductsSection() {
       <ProductCard />
       <ProductCard />
     </section>
+  );
+}
+
+function ProductCarousel() {
+  return (
+    <div className=" flex h-dvh w-full flex-col items-center justify-center bg-gradient-to-t  from-[#3f48788d] via-zinc-900  via-50% to-black pb-20 ">
+      <div className=" w-full">
+        <img src="/images/digital/Image_03.png" alt="" />
+      </div>
+      <div className="w-1/4 bg-black text-center text-5xl font-extralight">
+        <button className="mb-2 pr-4"> &#8249;</button>
+        <button className="mb-2 pl-4"> &#8250;</button>
+      </div>
+    </div>
+  );
+}
+// background-image:-webkit-linear-gradient(bottom , rgb(16, 16, 16) , rgba(0, 0, 0, 0) 51%);background-image:linear-gradient(to top , rgb(16, 16, 16) , rgba(0, 0, 0, 0) 51%)
+function PricingBox() {
+  return (
+    <div>
+      <div className=" p-5 gap-4 text-center  flex flex-col items-center justify-center  capitalize text-gray-300 ">
+        <p className="text-2xl ">play like the pros</p>
+        <h3 className="text-3xl font-bold uppercase text-white">
+          ready pc builds
+        </h3>
+        <p className='text-sm'>
+          There are many variations of passages of Lorem Ipsum available, but
+          the majority have suffered alteration in some form, by injected.{' '}
+        </p>
+      </div>
+      <div>
+        <PricingTable />
+        <PricingTable />
+        <PricingTable />
+      </div>
+    </div>
+  );
+}
+function PricingTable() {
+  return (
+    <div className="item-center mb-14 flex  w-full flex-col justify-center gap-2 text-center ">
+      <p className="py-2 text-xl font-semibold uppercase tracking-widest">
+        premium build
+      </p>
+      <div className="mx-5 border-2 border-indigo-600  bg-gray-900">
+        <div className=" bg-[url(/images/digital/Image_074.jpg)]  bg-cover bg-center py-4 font-bold">
+          <div className="text-4xl">$2500</div>
+          <div className="text-gray-300 ">per month</div>
+        </div>
+        <div>
+          <ul className="flex flex-col items-center justify-between   p-4 text-lg text-gray-300">
+            <li className="border- w-full  py-3">rayzen</li>
+            <li className="w-full border-t  py-3">rayzen</li>
+            <li className="w-full border-t  py-3">rayzen</li>
+            <li className="w-full border-t  py-3">rayzen</li>
+          </ul>
+          <div className=" border-t">
+            {' '}
+            <button className=" my-6 bg-indigo-600 px-5 py-3 uppercase">
+              buy now
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
