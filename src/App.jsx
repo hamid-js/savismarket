9d387c30599bc551caddc91e24326f8bf35822fa
Hamidreza Mohamadi <hamid.reactjs@gmail.com>
2024-01-27 16:55:07 +0330

    develop product

--- src/App.jsx
+++ src/App.jsx
@@ -127,57 +127,66 @@ function MobileMenu() {
 }
 
 function ProductCard({ isInAuctions }) {
+  const hoverStyle = 'hover:scale-125  hover:bg-indigo-700 hover:text-green-500';
   return (
     <div
-      className={`  mb-3 w-auto  min-w-64 text-white ${isInAuctions ? 'border border-gray-600' : 'border border-gray-600'}`}
+      className={` mb-3  w-64 text-white ${isInAuctions ? 'border border-gray-600' : 'border border-gray-600'}`}
     >
-      <div className="flex   h-44 w-full  flex-col justify-between items-end  bg-[url(/images/digital/gallery-27.jpg)] bg-cover bg-center  ">
-        <div className="mr-2 mt-1 h-7 w-7  rounded-full bg-black flex items-center justify-center ">
-          <RiHeartLine />
+      <div className="h-52 cursor-pointer bg-[url(/images/digital/gallery-27.jpg)] bg-cover  bg-center bg-no-repeat  duration-500 hover:bg-[url(/images/digital/gallery-26.jpg)]">
+        <div
+          className="flex h-5/6  w-full  flex-col items-end justify-center gap-4 
+         *:mr-2  *:flex *:h-8  *:w-8 *:items-center *:justify-center *:rounded-full *:bg-black *:duration-300  "
+        >
+          <div className={hoverStyle}>
+            <RiHeartLine />
+          </div>
+          <div className={hoverStyle}>
+            <RiSearchLine />
+          </div>
+          <div className={hoverStyle}>
+            <RiListCheck3 />
+          </div>
         </div>
-        <div className="mr-2 mt-1 h-8 w-8  rounded-full bg-black flex items-center justify-center">
-        <RiSearchLine />
-
-        </div>
-        <div className="mr-2 mt-1 h-8 w-8  rounded-full bg-black flex items-center justify-center">
-          <RiListCheck3  />
-        </div>
-        <div className="text- h-8 w-8 self-start rounded-sm bg-blue-500">
-          <RiShoppingCartLine className="-scale-x-100  pr-1  pt-2 text-2xl  " />
+        <div className="h-1/6">
+          <div className={`h-full mt-1  w-9 rounded-sm bg-blue-500 duration-300 ${hoverStyle}`}>
+            <RiShoppingCartLine className="-scale-x-100  pr-1  pt-2 text-3xl  " />
+          </div>
         </div>
       </div>
-      <div className='bg-zinc-800 mt-2'>
-      {isInAuctions ? (
-        <div className="flex  flex-col items-center justify-between gap-y-2 text-center capitalize">
-          <p>Time left</p>
+      <div className="mt-1 bg-zinc-800">
+        {isInAuctions ? (
+          <div className="flex  flex-col items-center justify-between gap-y-2 text-center capitalize">
+            <p>Time left</p>
 
-          <div className="flex w-full items-center justify-evenly border-b border-gray-700 pb-1">
-            <div>
-              175 <div className="text-[10px]  text-gray-400">days</div>
+            <div className="flex w-full items-center justify-evenly border-b border-gray-700 pb-1">
+              <div>
+                175 <div className="text-[10px]  text-gray-400">days</div>
+              </div>
+              <div>
+                13<div className="text-[10px]  text-gray-400">hours</div>
+              </div>
+              <div>
+                44<div className="text-[10px]  text-gray-400">minutes</div>
+              </div>
+              <div>
+                55<div className="text-[10px]  text-gray-400">seconds</div>
+              </div>
             </div>
-            <div>
-              13<div className="text-[10px]  text-gray-400">hours</div>
-            </div>
-            <div>
-              44<div className="text-[10px]  text-gray-400">minutes</div>
-            </div>
-            <div>
-              55<div className="text-[10px]  text-gray-400">seconds</div>
-            </div>
+
+            <p className="font-semibold">duel sim Smartphone</p>
+            <p className="text-sm font-light *:text-lg *:font-semibold">
+              current bid: <span>$411.00</span>
+            </p>
           </div>
-
-          <p className="font-semibold">duel sim Smartphone</p>
-          <p className="text-sm font-light *:text-lg *:font-semibold">
-            current bid: <span>$411.00</span>
-          </p>
-        </div>
-      ) : (
-        <div className="flex flex-col items-center  ">
-          <p className="font-semibold text-lg mt-1">Smartphone</p>
-          <p className="text-gray-300 text-sm">duel sim</p>
-          <p className="text-blue-500 font-bold text-xl tracking-widest">$65.46</p>
-        </div>
-      )}
+        ) : (
+          <div className="flex flex-col items-center  ">
+            <p className="mt-1 text-lg font-semibold">Smartphone</p>
+            <p className="text-sm text-gray-300">duel sim</p>
+            <p className="text-xl font-bold tracking-widest text-blue-500">
+              $65.46
+            </p>
+          </div>
+        )}
       </div>
     </div>
   );
@@ -435,7 +444,7 @@ function Footer() {
 function LaptopsSection() {
   return (
     <div className="bg-gradient-to-t from-zinc-950  via-zinc-800 via-50% to-zinc-950 p-5">
-      <div className="flex mb-2 h-48 w-full flex-col items-center justify-center  gap-y-5 bg-gradient-to-tr from-blue-700 via-blue-500 to-blue-700  uppercase">
+      <div className="mb-2 flex h-48 w-full flex-col items-center justify-center  gap-y-5 bg-gradient-to-tr from-blue-700 via-blue-500 to-blue-700  uppercase">
         <h2 className="font-bold ">phones</h2>
         <div>
           <span>12 </span>Products
