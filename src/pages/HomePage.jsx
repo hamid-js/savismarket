930051ccd61922871ffd9e341c31565d295f62ef
Hamidreza Mohamadi <hamid.reactjs@gmail.com>
2024-02-06 01:31:32 +0330

    cart logic done complately2

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -9,40 +9,35 @@ import Auctions from '../components/products/Auctions';
 import AllCategories from '../components/products/AllCategories';
 import { Outlet } from 'react-router-dom';
 
-
-
 function HomePage() {
-    return (
-        <div>
-           <Outlet />
+  return (
+    <div>
+      <Outlet />
       <main className="pb-10">
         <HiroSilder />
-        
+
         <Services />
         <Auctions />
         <AllCategories />
-        <ProductSlider /> 
-        <PricingBox /> 
-        <ArticlesCards /> 
+        <ProductSlider />
+        <PricingBox />
+        <ArticlesCards />
         <Gallery />
       </main>
-      
-        </div>
-    )
+    </div>
+  );
 }
 
-export default HomePage
+export default HomePage;
 //  color: select a palet and change all colors
-    //  menu:  menu page 
-    // install react router dom  create all pages that need
-    // popup: products preview popup
-    //  routes:  complate all pages that need
-        //  pages: product detalis,products,blog,login
+//  menu:  menu page
+// install react router dom  create all pages that need
+// popup: products preview popup
+//  routes:  complate all pages that need
+//  pages: product detalis,products,blog,login
+// Learn: learn {use context api} for a store (gpt + jonas + sabs + youtube)
+// cart: adding product to cart logic +
 
-    //TODO  Learn: learn {use context api} for a store (gpt + jonas + sabs + youtube)
-
-    //TODO  time: products auction times logic
-    //TODO  cart: adding product to cart logic + toast(by third-parti)
-
-    
-    //TODO  pages: compare,wishlist,product after clicks page,checkout , contact 
+//TODO  time: products auction times logic
+//TODO  toast(by third-parti)
+//TODO  pages: compare,wishlist,product after clicks page,checkout , contact
