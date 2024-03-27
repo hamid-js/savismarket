d3b8c17ebe94c62264797400e92c9b6ea21bcb37
Hamidreza Mohamadi <hamid.reactjs@gmail.com>
2024-03-28 02:53:46 +0330

    responsive 4

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -23,7 +23,7 @@ function Cart() {
       }}
       className="top fixed right-0 z-50 h-dvh w-[75vw] max-w-96 overflow-y-hidden rounded-sm bg-slate-800  text-slate-300 duration-300"
     >
-      <div className="flex justify-between shadow-sm mb-1 shadow-slate-400 border-bborder-zinc-700 px-3 py-1 text-[4.5vw] font-semibold sm:text-2xl ">
+      <div className="border-bborder-zinc-700 mb-1 flex justify-between px-3 py-1 text-[4.5vw] font-semibold shadow-sm shadow-slate-400 sm:text-2xl ">
         <p className=" py-3  capitalize ">Shopping Cart</p>
         <div
           onClick={() => setIsOpenCart(false)}
@@ -34,7 +34,7 @@ function Cart() {
         </div>
       </div>
       {!cart.length ? (
-        <div className="flex h-2/3 flex-col items-center justify-center gap-5 text-[4vw] sm:text-xl font-bold">
+        <div className="flex h-2/3 flex-col items-center justify-center gap-5 text-[4vw] font-bold sm:text-xl">
           <MdOutlineRemoveShoppingCart className="text-9xl text-zinc-900" />
 
           <p>No Products in the Cart.</p>
@@ -46,7 +46,7 @@ function Cart() {
         </div>
       ) : (
         <div>
-          <div className=" h-[52vh] overflow-y-scroll bg-slate-900 border-b border-zinc-700">
+          <div className=" h-[52vh] overflow-y-scroll border-b border-zinc-700 bg-slate-900">
             {cart &&
               cart.map((product) => (
                 <CartItem
@@ -61,12 +61,12 @@ function Cart() {
                 />
               ))}
           </div>
-          <div className='absolute bottom-0 w-full'>
-          <div className="mb-3 flex max-h-12 pb-5 items-center justify-between border-b border-zinc-700 px-3 text-xl font-semibold">
-            <div>Subtotal:</div>
-            <div className="text-indigo-600">${totalPrice}</div>
-          </div>
-       
+          <div className="absolute bottom-0 w-full">
+            <div className="mb-3 flex max-h-12 items-center justify-between border-b border-zinc-700 px-3 pb-5 text-xl font-semibold">
+              <div>Subtotal:</div>
+              <div className="text-indigo-600">${totalPrice}</div>
+            </div>
+
             <div className=" flex h-auto flex-col justify-center  gap-1  px-[1vw]">
               <div className=" text-[3.4vw] sm:text-xl">
                 {totalPrice > 10000 ? (
@@ -87,21 +87,28 @@ function Cart() {
                 )}
               </div>
               <div>
-              <div className=" my-3 h-3 bg-zinc-700">
-                <div
-                  style={{
-                    width: `${totalPrice < 10000 ? (totalPrice / 10000) * 100 : 100}%`,
-                  }}
-                  className="h-full  bg-indigo-500"
-                ></div>
+                <div className=" my-3 h-3 bg-zinc-700">
+                  <div
+                    style={{
+                      width: `${totalPrice < 10000 ? (totalPrice / 10000) * 100 : 100}%`,
+                    }}
+                    className="h-full  bg-indigo-500"
+                  ></div>
+                </div>
+                <div>
+                  {' '}
+                  {totalPrice < 10000
+                    ? Math.floor((totalPrice / 10000) * 100) + ' %'
+                    : '✅'}
+                </div>
               </div>
-             <div> {totalPrice < 10000 ? Math.floor((totalPrice / 10000) * 100 ) + " %": "✅"}</div>
-             </div>
               <div className="flex flex-col ">
-                <Button style={{ backgroundColor: 'black' }}> view cart</Button>
-
+                <Link onClick={() => setIsOpenCart(false)} to={'/shop'}>
+                  <Button style={{ backgroundColor: 'black' }}>
+                    back to shop
+                  </Button>
+                </Link>
                 <Link onClick={() => setIsOpenCart(false)} to={'/checkout'}>
-                
                   <Button>checkout</Button>
                 </Link>
               </div>
