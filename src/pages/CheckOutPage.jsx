5590fa2e9ee0036a2f49bc68170f4591ffe2fda2
Hamidreza Mohamadi <hamid.reactjs@gmail.com>
2024-03-29 03:23:35 +0330

    responsive 17

--- src/pages/CheckOutPage.jsx
+++ src/pages/CheckOutPage.jsx
@@ -13,82 +13,85 @@ function CheckoutPage() {
         return a + b;
       });
   return (
-    <div className=' py-10 bg-gradient-to-t from-slate-800 pt-40 via-slate-700  via-50% to-slate-900 px-3 '>
-      <div className='border-dashed border px-4 py-7 mx-2 '>
-      <div className=" text-lg mb-5">
-                {totalPrice > 10000 ? (
-                  <p>
-                    Your order qualifies for
-                    <span className="font-bold"> free shipping! </span>
-                  </p>
-                ) : (
-                  <p>
-                    Add
-                    <span className="text-indigo-400 ">
-                      {' '}
-                      ${10000 - totalPrice}{' '}
-                    </span>
-                    to cart and get
-                    <span className="font-bold"> free shipping! </span>
-                  </p>
-                )}
-              </div>
-        <div className="  h-3 bg-zinc-700">
-          <div
-            style={{
-              width: `${totalPrice < 10000 ? (totalPrice / 10000) * 100 : 100}%`,
-            }}
-            className="h-full  bg-indigo-500"
-          ></div>
-        </div>
-      </div>
+    <div className=" bg-gradient-to-t from-slate-800 via-slate-700 via-50% to-slate-900  px-3 py-10 pt-40 ">
 
+      <div className="mx-auto sm:right-[12%]  sm:top-[5dvh] sm:w-9/12 lg:right-[20%]  lg:top-[2dvh] lg:w-7/12 xl:right-[27%] xl:w-5/12">
 
-      <div>
-      <div>
-        <LoginForm
-          firstInput="Username or email address"
-          secondInput="Password"
-          btnText="log in"
-          title="Login"
-          addText="Lost your password?"
-          remember
-        />
-        <LoginForm
-          title="Register"
-          firstInput="Username "
-          secondInput="email address"
-          thirdInput="Password"
-          btnText="Register"
-        />
-      </div>
-      </div>
-
-      <div>
-        <div className=' text-center py-3 font-semibold text-stone-100 uppercase text-2xl'>your order</div>
-        <div className='bg-zinc-700 '>
-        <div className="p-5  border-b border-zinc-700">
-            {cart &&
-              cart.map((product) => (
-                <CartItem
-                  key={product.id}
-                  id={product.id}
-                  name={product.name}
-                  firstImage={product.firstImage}
-                  hoverImage={product.hoverImage}
-                  price={product.price}
-                  details={product.details}
-                  orderCount={product.orderCount}
-                />
-              ))}
+        <div className="mx-2 border border-dashed px-4 py-7 ">
+          <div className=" mb-5 text-lg">
+            {totalPrice > 10000 ? (
+              <p>
+                Your order qualifies for
+                <span className="font-bold"> free shipping! </span>
+              </p>
+            ) : (
+              <p>
+                Add
+                <span className="text-indigo-400 ">
+                  {' '}
+                  ${10000 - totalPrice}{' '}
+                </span>
+                to cart and get
+                <span className="font-bold"> free shipping! </span>
+              </p>
+            )}
           </div>
-        <div className=" flex h-[10vh] items-center justify-between border-b border-zinc-700 p-5  text-xl font-semibold">
-            <div>Subtotal:</div>
-            <div className="text-indigo-600">${totalPrice}</div>
+          <div className="  h-3 bg-zinc-700">
+            <div
+              style={{
+                width: `${totalPrice < 10000 ? (totalPrice / 10000) * 100 : 100}%`,
+              }}
+              className="h-full  bg-indigo-500"
+            ></div>
           </div>
         </div>
+        <div className='border border-dotted my-5'>
+          <div className=" bg-slate-900 py-3 text-center text-2xl font-semibold uppercase text-stone-100">
+            your order
+          </div>
+          <div className="bg-zinc-700 ">
+            <div className="border-b  border-zinc-700 p-5">
+              {cart &&
+                cart.map((product) => (
+                  <CartItem
+                    key={product.id}
+                    id={product.id}
+                    name={product.name}
+                    firstImage={product.firstImage}
+                    hoverImage={product.hoverImage}
+                    price={product.price}
+                    details={product.details}
+                    orderCount={product.orderCount}
+                  />
+                ))}
+            </div>
+            <div className=" flex h-[10vh] items-center justify-between border-b border-zinc-700 p-5  text-xl font-semibold">
+              <div>Subtotal:</div>
+              <div className="text-indigo-600">${totalPrice}</div>
+            </div>
+          </div>
+        </div>
+        <div className=' bg-[url(/images/bg/bg4.jpg)] bg-cover my-7'>
+          <div>
+            <LoginForm
+              firstInput="Username or email address"
+              secondInput="Password"
+              btnText="log in"
+              title="Login"
+              addText="Lost your password?"
+              remember
+            />
+            <LoginForm
+              title="Register"
+              firstInput="Username "
+              secondInput="email address"
+              thirdInput="Password"
+              btnText="Register"
+            />
+          </div>
+        </div>
+
         
-
       </div>
     </div>
   );
--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -2,7 +2,8 @@ import LoginForm from '../components/ui/LoginForm';
 
 function ContactPage() {
   return (
-    <div className="bg-[url(/images/bg/bg4.jpg)] bg-cover ">
+    <div className='bg-gradient-to-bl from-slate-900 via-slate-600 to-slate-900 py-20 '>
+    <div className="border-2 my-20 bg-[url(/images/bg/bg4.jpg)] bg-cover h-[60dvh] mx-5 sm:right-[12%] sm:top-[5dvh] sm:mx-auto  sm:h-[70dvh] lg:sm:h-[85dvh] sm:w-9/12 lg:right-[20%] lg:top-[2dvh]  lg:w-7/12 xl:right-[27%] xl:w-5/12">
       <div>
         <LoginForm
           firstInput="Your Name"
@@ -13,6 +14,7 @@ function ContactPage() {
           addText="We will contact you soon."
         />
       </div>
+    </div>
     </div>
   );
 }
